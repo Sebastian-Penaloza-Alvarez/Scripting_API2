87445c82ab1eb9b01eaae90063f6f548fcc73941
Sebastian Peñaloza Alvarez <sebapa341@gmail.com>
2026-07-02 23:22:37 -0400

    Resolución Actividad 2 API 2

---
+++ API2_menu_promocional.py
@@ -0,0 +1,11 @@
+#Operación 1: Suma
+resultado_suma = 10 + 3
+print("Total suma:",(float(resultado_suma)))
+
+#Operación 2: División
+resultado_division = 10 / 3
+print("Total división:",(int(resultado_division)))
+
+#Operación 3: Multiplicación
+resultado_multiplicar = 5 * 2
+print("Total multiplicación:",(float(resultado_multiplicar)))
